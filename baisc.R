bd48752040f5bd727ab1946fad1a2a481359d428
devsunset <devsunset@gmail.com>
2023-10-28 11:50:59 +0900

    dev

--- baisc.R
+++ baisc.R
@@ -3,6 +3,64 @@
 library(dplyr)
 #help("dplyr")
 
+# install.packages("psych")
+# install.packages("descr")
+# install.packages("gmodels")
+# install.packages("MASS")
+# install.packages("Hmisc")
+# install.packages("dplyr")
+# install.packages("fBasics")
+# install.packages("QuantPsyc")
+# install.packages("prettyR")
+# install.packages("e1071")
+# install.packages('abind')
+# install.packages('zoo')
+# install.packages('xts')
+# install.packages('quantmod')
+# install.packages('ROCR')
+# install.packages("https://cran.r-project.org/src/contrib/Archive/DMwR/DMwR_0.4.1.tar.gz", repos = NULL, type="source")
+# install.packages("rpart")
+# install.packages("rpart.plot")
+# install.packages("party")
+# install.packages("nnet")
+# install.packages("downloader")
+# install.packages("neuralnet")
+# install.packages("tree")
+# install.packages("caret")
+# install.packages("arules")
+# install.packages("arulesViz")
+# install.packages("lattice")
+# install.packages("topicmodels")
+# install.packages("wordcloud")
+# install.packages("RColorBrewer")
+# install.packages("sna")
+# install.packages("igraph")
+# install.packages("rgl")
+# install.packages("rvest")
+# install.packages("stringr")
+# install.packages("XML")
+# install.packages("readxl")
+# install.packages("WriteXLS")
+# # rJava 설치
+# install.packages("multilinguer")	
+# library(multilinguer)
+# install_jdk()
+# # KoNLP 패키지 설치시 참조 또는 이용되는 패키지 설치
+# install.packages(c("hash", "tau", "Sejong", "RSQLite", "devtools", "bit", "rex", "lazyeval", "htmlwidgets", "crosstalk", "promises", "later", "sessioninfo", "xopen", "bit64", "blob", "DBI", "memoise", "plogr", "covr", "DT", "rcmdcheck", "rversions"), type = "binary")
+# # 깃허브의 KoNLP 패키지 설치
+# install.packages("remotes")
+# remotes::install_github('haven-jeon/KoNLP', upgrade = "never", INSTALL_opts=c("--no-multiarch"))
+# # 텍스트마이닝을 위한 tm 패키지 설치
+# require(remotes)
+# install_version("tm", version = "0.7-5", repos = "http://cran.us.r-project.org")
+# install.packages("ggplot2")
+# install.packages("treemap")
+# install.packages("aplpack")
+# install.packages("raster")
+# install.packages("rgeos")
+# install.packages("rgdal")
+# install.packages("ggmap")
+
 x <- "Hello World"
 x
 
@@ -292,3 +350,18 @@ order(-x)
 sort(-x)
 quantile(x)
 IQR(x)
+
+## 데이터세트 
+data()
+quakes
+help(quakes)
+names(quakes)
+head(quakes)
+quakes[3,4]
+quakes[5,1]
+quakes$depth[4]
+quakes$mag[6]
+quakes$depth[1]+quakes$depth[2]
+
+### 구조적 프로그래밍 
+
--- readme.txt
+++ readme.txt
@@ -20,67 +20,6 @@ https://www.yes24.com/Product/Goods/119487224
 https://wikidocs.net/book/4315
 
 ########################################################
-### Install Package
-
-# install.packages("psych")
-# install.packages("descr")
-# install.packages("gmodels")
-# install.packages("MASS")
-# install.packages("Hmisc")
-# install.packages("dplyr")
-# install.packages("fBasics")
-# install.packages("QuantPsyc")
-# install.packages("prettyR")
-# install.packages("e1071")
-# install.packages('abind')
-# install.packages('zoo')
-# install.packages('xts')
-# install.packages('quantmod')
-# install.packages('ROCR')
-# install.packages("https://cran.r-project.org/src/contrib/Archive/DMwR/DMwR_0.4.1.tar.gz", repos = NULL, type="source")
-# install.packages("rpart")
-# install.packages("rpart.plot")
-# install.packages("party")
-# install.packages("nnet")
-# install.packages("downloader")
-# install.packages("neuralnet")
-# install.packages("tree")
-# install.packages("caret")
-# install.packages("arules")
-# install.packages("arulesViz")
-# install.packages("lattice")
-# install.packages("topicmodels")
-# install.packages("wordcloud")
-# install.packages("RColorBrewer")
-# install.packages("sna")
-# install.packages("igraph")
-# install.packages("rgl")
-# install.packages("rvest")
-# install.packages("stringr")
-# install.packages("XML")
-# install.packages("readxl")
-# install.packages("WriteXLS")
-# # rJava 설치
-# install.packages("multilinguer")	
-# library(multilinguer)
-# install_jdk()
-# # KoNLP 패키지 설치시 참조 또는 이용되는 패키지 설치
-# install.packages(c("hash", "tau", "Sejong", "RSQLite", "devtools", "bit", "rex", "lazyeval", "htmlwidgets", "crosstalk", "promises", "later", "sessioninfo", "xopen", "bit64", "blob", "DBI", "memoise", "plogr", "covr", "DT", "rcmdcheck", "rversions"), type = "binary")
-# # 깃허브의 KoNLP 패키지 설치
-# install.packages("remotes")
-# remotes::install_github('haven-jeon/KoNLP', upgrade = "never", INSTALL_opts=c("--no-multiarch"))
-# # 텍스트마이닝을 위한 tm 패키지 설치
-# require(remotes)
-# install_version("tm", version = "0.7-5", repos = "http://cran.us.r-project.org")
-# install.packages("ggplot2")
-# install.packages("treemap")
-# install.packages("aplpack")
-# install.packages("raster")
-# install.packages("rgeos")
-# install.packages("rgdal")
-# install.packages("ggmap")
-
-########################################################
 ### Basic
 
 basic.R
