359e857cffac05b26f0e27eb05823e7d6f839655
devsunset <devsunset@gmail.com>
2023-10-28 13:46:17 +0900

    dev

--- dataCollectAndAnalysis.R
+++ dataCollectAndAnalysis.R
@@ -1,2 +1,33 @@
-x <- "Hello R"
-x
+#install.packages("readxl")
+#install.packages("psyeh")
+library(readxl)
+library(psych)
+
+# sample_data <- data.frame()
+# sample_data <- edit(sample_data)
+
+# setwd()
+getwd()
+
+options(width="500")
+data <- read.csv("data/data.csv", header=T, fileEncoding="EUC-KR")
+data
+head(data)
+summary(data)
+edit(data)
+
+data <- read_excel("data/pollution_air.xlsx",sheet="pollution_air", col_names=TRUE)
+data
+head(data)
+summary(data)
+
+AirPassengers
+iris
+mtcars
+Titanic
+state.x77
+women
+
+install.packages("MASS")
+library(MASS)
+Boston
