1a8bdddf309ef368765d94375c8bc6a7e43bc106
devsunset <devsunset@gmail.com>
2023-10-28 09:37:54 +0900

    dev

--- baisc.R
+++ baisc.R
@@ -1,70 +1,10 @@
+### R 기초 
+
 library(dplyr)
 #help("dplyr")
 
 x <- "Hello World"
 x
-
-############################################################################
-# install.packages("psych")
-# install.packages("descr")
-# install.packages("gmodels")
-# install.packages("MASS")
-# install.packages("Hmisc")
-# install.packages("dplyr")
-# install.packages("fBasics")
-# install.packages("QuantPsyc")
-# install.packages("prettyR")
-# install.packages("e1071")
-# install.packages('abind')
-# install.packages('zoo')
-# install.packages('xts')
-# install.packages('quantmod')
-# install.packages('ROCR')
-# install.packages("https://cran.r-project.org/src/contrib/Archive/DMwR/DMwR_0.4.1.tar.gz", repos = NULL, type="source")
-# install.packages("rpart")
-# install.packages("rpart.plot")
-# install.packages("party")
-# install.packages("nnet")
-# install.packages("downloader")
-# install.packages("neuralnet")
-# install.packages("tree")
-# install.packages("caret")
-# install.packages("arules")
-# install.packages("arulesViz")
-# install.packages("lattice")
-# install.packages("topicmodels")
-# install.packages("wordcloud")
-# install.packages("RColorBrewer")
-# install.packages("sna")
-# install.packages("igraph")
-# install.packages("rgl")
-# install.packages("rvest")
-# install.packages("stringr")
-# install.packages("XML")
-# install.packages("readxl")
-# install.packages("WriteXLS")
-# # rJava 설치
-# install.packages("multilinguer")	
-# library(multilinguer)
-# install_jdk()
-# # KoNLP 패키지 설치시 참조 또는 이용되는 패키지 설치
-# install.packages(c("hash", "tau", "Sejong", "RSQLite", "devtools", "bit", "rex", "lazyeval", "htmlwidgets", "crosstalk", "promises", "later", "sessioninfo", "xopen", "bit64", "blob", "DBI", "memoise", "plogr", "covr", "DT", "rcmdcheck", "rversions"), type = "binary")
-# # 깃허브의 KoNLP 패키지 설치
-# install.packages("remotes")
-# remotes::install_github('haven-jeon/KoNLP', upgrade = "never", INSTALL_opts=c("--no-multiarch"))
-# # 텍스트마이닝을 위한 tm 패키지 설치
-# require(remotes)
-# install_version("tm", version = "0.7-5", repos = "http://cran.us.r-project.org")
-# install.packages("ggplot2")
-# install.packages("treemap")
-# install.packages("aplpack")
-# install.packages("raster")
-# install.packages("rgeos")
-# install.packages("rgdal")
-# install.packages("ggmap")
-############################################################################
-
-### R 기초 
 
 ## 데이타 유형 및 변수 할당 
 # numeric integer , double 
@@ -286,5 +226,38 @@ z <- rbind(a,b)
 z
 x <- c("a","d","x","b")
 ls(x)
-
-
+a <- c(1,"male",3,"female")
+mode(a)
+a[1] + a[3]
+as.numeric(a[1])+as.numeric(a[3])
+a <- "Hello"
+b <- "World"
+z <- paste(a,b)
+z
+na.omit()
+a <- c(1,2,3,4)
+mode(a)
+z <- paste(as.character(a[1]),as.character(a[3]))
+z
+a <- 0
+as.logical(a)
+a <- 1
+as.logical(a)
+a <- 2
+as.logical(a)
+a <- matrix(c(1,2,3,5),nrow=2)
+a
+a <- as.vector(a)
+a
+as.matrix(1:10)
+matrix(1:12,nrow=3)
+a <- c(1,2,3,5)
+b <- c("a","b","c","d")
+z <- as.data.frame(a,b)
+z
+z <- data.frame(a,b)
+z
+a <- c(1,2,3,4)
+a
+a <- append(a,c(5,6,7,8))
+a
