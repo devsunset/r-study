219baf34e37fcb75096961590cca9815ee6f74a9
devsunset <devsunset@gmail.com>
2023-11-27 10:41:56 +0900

    dev

--- exam.R
+++ exam.R
@@ -118,4 +118,73 @@ f_sd <- sd(f)
 z_score <- (f-f_mean)/f_sd
 z_score
 
+#########################################################################
+a <- read.csv("data/mtcars.csv")
+# print(a)
+# print(describe(a))
+# print(summary(a))
+# print(str(a))
+# print(dim(a))
+data <- a[order(-a$mpg), ]
+data <- data[1:10,]
+c2_hp <- mean(data$hp[data$carb == 2])
+c1_hp <- mean(data$hp[data$carb == 1])
+print(c2_hp - c1_hp)
 
+#########################################################################
+a <- read.csv("data/mtcars.csv")
+data <- subset(a, am == 1 & cy1 == 4)
+nrow(data)
+result <- mean(data$mpg)+sd(data$hp)
+print(result)
+data <- a %>% filter(am == 1 & cy1 == 4)
+print(mean(data$mpg)+sd(data$hp))
+
+#########################################################################
+a <- read.csv("data/mtcars.csv")
+data <- subset(a, am == 0)
+print(data)
+print(nrow(data))
+# print(boxplot(a))
+q <- quantile(data$mpg)
+print(q)
+print(q[4] - q[2])
+iqr <- IQR(data$mpg)
+print(iqr)
+r1 <- mean(data$mpg) - iqr
+print(r1)
+r2 <- mean(data$mpg) + iqr
+print(r2)
+result <- data$mpg <= r1 | data$mpg >= r2
+print(result)
+print(sum(data$mpg[result]))
+print(sum(result))
+print(sum(data$mpg[result])/sum(result))
+
+#########################################################################
+# head(iris)
+# dim(iris)
+# print(summary(iris))
+# print(describe(iris))
+print(mean(iris$Petal.Length)+sd(iris$Petal.Length))
+print(mean(iris$Petal.Width)+sd(iris$Petal.Width))
+data <- subset(iris,Species == 'setosa')
+# print(data)
+maxpl <- max(data$Petal.Length)
+print(maxpl)
+minpl <- min(data$Petal.Length)
+print(minpl)
+minmax <- function(x){
+	return ((x-min(x))/(max(x)-min(x)))
+}
+data$Petal.Length <- minmax(data$Petal.Length)
+# print(summary(data))
+print(mean(data$Petal.Length))
+
+data <- subset(iris,Species == 'setosa')
+zscore <- function (x) {
+	return ((x-mean(x))/sd(x))
+}
+data$Petal.Length <- zscore(data$Patal.Length)
+# print(summary(data))
+print(mean(data$Petal.Length))
