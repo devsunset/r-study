9a960a99b1cae2cf25d2494872efc9900ee2c7e1
devsunset <devsunset@gmail.com>
2023-11-30 10:52:40 +0900

    dev

--- exam.R
+++ exam.R
@@ -21,3 +21,29 @@ check_mean <- mean(check$crim)
 print(check_mean)
 
 #########################################################################
+data <- read.csv("data/housing.csv",header=T)
+head(data)
+dim(data)
+str(data)
+summary(data)
+
+n <- nrow(data) * 0.8
+n
+
+data1 <- data[c(1:n),]
+dim(data1)
+summary(data1)
+
+sd_data1 <- sd(data1$total_bedrooms, na.rm = T)
+print(sd_data1)
+
+median <- median(data1$total_bedrooms, na.rm = T)
+print(median)
+
+data1$total_bedrooms <- ifelse(is.na(data1$total_bedrooms), median, data1$total_bedrooms)
+summary(data1)
+
+sd_data1 <- sd(data1$total_bedrooms)
+print(sd_data1)
+
+
